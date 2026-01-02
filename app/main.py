e216e39ef10106c27f128a573573efe10ced0259
Darlin Estrada Patino <darlin.estrada456@gmail.com>
2026-01-02 13:13:30 -0500

    middleware para medir tiempos de request y ver los headers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import zoneinfo
+import time
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from db import create_all_tables
 from .routers import customers, transactions, invoices, plans
 
@@ -11,7 +12,19 @@ app.include_router(transactions.router)
 app.include_router(invoices.router)
 app.include_router(plans.router)
 
+@app.middleware("http")
+async def log_request_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"Request: {request.url} completed in: {process_time:.4f} seconds")
+    return response
 
+@app.middleware("http")
+async def log_request_headers(request: Request, call_next):
+    # print(f"Request: {request.url} headers: {request.headers}")
+    response = await call_next(request)
+    return response
 
 @app.get("/")
 async def root():
@@ -28,7 +41,7 @@ country_timezones = {
 
 
 @app.get("/time/{iso_code}")
-async def time(iso_code: str):
+async def get_time_by_iso_code(iso_code: str):
     iso = iso_code.upper()
     timezone_str = country_timezones.get(iso)
     if timezone_str is None:
